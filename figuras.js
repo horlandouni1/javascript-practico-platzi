e2d958ce5c4d15587fa7a34edd1b82aaf2bdf35c
horlandouni1 <orland_1998@hotmail.com>
2021-10-07 00:12:35 -0500

    se agrega calculos de figuras matematicas

---
+++ figuras.js
@@ -0,0 +1,16 @@
+console.group("triangulo");
+const lado_cuadrado=5;
+console.log("los lados del cuadrado miden: "+ lado_cuadrado);
+const perimetro_cuadrado=lado_cuadrado*4;
+console.log("el perimetro del cuadrado es :" + perimetro_cuadrado);
+const area_cuadrado=lado_cuadrado*lado_cuadrado;
+console.log("el area del cuadrado es :" + area_cuadrado);
+console.groupEnd();
+console.group("cuadrado")
+const ladotriangulo1=6;
+const ladotriangulo2=6
+const ladotriangulo3=10;
+const perimetro_triangulo=ladotriangulo1+ladotriangulo2+ladotriangulo3;
+console.log("el perimetro del triangulo es: "+perimetro_triangulo);
+console.groupEnd();
+
